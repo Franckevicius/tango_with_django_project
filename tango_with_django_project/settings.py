8fa090c979b4a9eebd498dc28cae2c5d3eeb75c1
Benas Franckevicius <2550515f@student.gla.ac.uk>
2022-01-16 18:38:07 +0200

    Fix folder hierarchy

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -13,11 +13,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
-print(BASE_DIR)
-print(TEMPLATE_DIR)
-
 
 
 # Quick-start development settings - unsuitable for production
