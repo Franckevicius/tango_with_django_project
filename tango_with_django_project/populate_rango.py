168bf6151759f12a88a740339ed085179e6c1ace
Benas Franckevicius <2550515f@student.gla.ac.uk>
2022-01-21 11:35:27 +0200

    Add category views and likes, update population script

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -30,9 +30,15 @@ def populate():
         {'title': 'Flask',
          'url': 'http://flask.pocoo.org'}]
 
-    categories = {'Python': {'pages': python_pages},
-                  'Django': {'pages': django_pages},
-                  'Other Frameworks': {'pages': other_pages}}
+    categories = {'Python': {'pages': python_pages, 
+                             "views" : 128, 
+                             "likes" : 64},
+                  'Django': {'pages': django_pages,
+                             "views": 64,
+                             "likes": 32},
+                  'Other Frameworks': {'pages': other_pages,
+                                       "views": 32,
+                                       "likes": 16}}
 
 
     def add_page(cat, title, url, views=0):
@@ -42,13 +48,13 @@ def populate():
         p.save()
         return p
 
-    def add_cat(name):
-        c = Category.objects.get_or_create(name=name)[0]
+    def add_cat(name, views, likes):
+        c = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
         c.save()
         return c
 
     for cat, cat_data in categories.items():
-        c = add_cat(cat)
+        c = add_cat(cat, cat_data["views"], cat_data["likes"])
         for p in cat_data['pages']:
             add_page(c, p['title'], p['url'])
     
